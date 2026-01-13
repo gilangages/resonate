dd2eb938410b5971bfab1256986a5cbadb586863
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-01-13 23:49:13 +0700

    edit again note-api.json

--- front-end/src/lib/api/NoteApi.js
+++ front-end/src/lib/api/NoteApi.js
@@ -1,1 +1,21 @@
-export const noteCreate = async();
+export const noteCreate = async (
+  token,
+  { content, recipient, spotify_track_id, spotify_track_name, spotify_artist, spotify_album_image }
+) => {
+  return await fetch(`${import.meta.env.VITE_APP_PATH}/notes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      content,
+      recipient,
+      spotify_track_id,
+      spotify_track_name,
+      spotify_artist,
+      spotify_album_image,
+    }),
+  });
+};
