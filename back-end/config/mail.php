c43ff495f703b0b2947ad970c89fa95547c694b1
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-01-17 19:34:08 +0700

    use berevo for email

--- back-end/config/mail.php
+++ back-end/config/mail.php
@@ -39,7 +39,6 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => null,
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
@@ -47,14 +46,6 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-
-            'context' => [
-                'ssl' => [
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true,
-                ],
-            ],
 
             'stream' => [
                 'ssl' => [
