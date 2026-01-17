01cd4940e5af6e5a22f7e48801c283d717c0a797
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-01-17 11:07:38 +0700

    Add deploy script and switch to Apache

---
+++ back-end/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 1. Jalankan Migrasi Database
+echo "Running Migrations..."
+php artisan migrate --force
+
+# 2. Jalankan Seeder (Super Admin)
+echo "Running Seeder..."
+php artisan db:seed --class=SuperAdminSeeder --force
+
+# 3. Optimasi Config & Route (Biar Ringan)
+echo "Caching Configuration..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# 4. Jalankan Web Server Apache (Production Grade)
+echo "Starting Apache..."
+exec apache2-foreground
