7b5942240ed4c1eec76c50c841cda3cff59e37cf
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-01-17 17:42:37 +0700

    samaran-svg-4

--- front-end/src/components/LandingPage/Section/Fitur.vue
+++ front-end/src/components/LandingPage/Section/Fitur.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from "vue";
-import { anonimMode } from "../../../../public/samaran.svg";
 
 // --- ICON ASSETS (SVG Strings) ---
 
@@ -25,7 +24,7 @@ const features = ref([
   {
     title: "Mode Anonim",
     desc: "Ingin menyampaikan perasaan kepada seseorang tapi tak ingin identitasmu diketahui? Gunakan mode anonim (inisial) agar kamu bisa bebas bercerita tanpa rasa khawatir.",
-    img: anonimMode,
+    img: "./../../../public/personal.svg",
     isGeneric: false,
   },
   {
